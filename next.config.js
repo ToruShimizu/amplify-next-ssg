d308608f2ca5d75e9e07f7e014348a4678fccb92
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-07-05 19:15:05 +0900

    fix: リライト

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   output: 'export',
+  rewrites: async () => [
+    {
+      source: '/foo/:id',
+      destination: '/foo/:id',
+    },
+  ],
 }
 
 module.exports = nextConfig
