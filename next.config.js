26dbee57001dc98d2b31e03585b3422635a25b82
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-07-05 19:22:39 +0900

    fix: リライト

--- next.config.js
+++ next.config.js
@@ -1,14 +1,28 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   output: 'export',
-  rewrites: async () => [
-    {
-      source: '/foo/:id',
-      destination: '/foo/:id',
-    },
-  ],
 }
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    /** @type {import('next').NextConfig} */
+    return {
+      ...nextConfig,
+      async rewrites() {
+        return {
+          beforeFiles: [
+            {
+              source: "/foo/:id",
+              destination: "/foo/:id",
+            },
+          ],
+        };
+      },
+    };
+  }
 
-module.exports = nextConfig
+  return nextConfig;
+};
